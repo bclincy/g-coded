fadbf0b4c0d11bc180167dfca590236c4d3f0371
Danielle Anderson <msdanderson24@gmail.com>
2023-08-14 12:25:19 -0400

    updated todo list added js

---
+++ week2/script.js
@@ -0,0 +1,5 @@
+var add=document.getElementById("add ToDo");
+var input=document.getelementById("Inputfield");
+var toDoContainer=document.getElementById("toDoContainer");
+add.addEventListener('click',additem);
+input.addEventListener('keypress',)
